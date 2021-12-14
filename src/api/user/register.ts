90d15a985dc9dd6c7e4e307f8429bc61ce3f39f1
Виталий Павленко <pavlenko.v.ale@sberbank.ru>
2021-12-14 17:05:04 +0300

    валюта выставляется в зависимости от локализации

--- src/api/user/register.ts
+++ src/api/user/register.ts
@@ -4,12 +4,12 @@ interface RegisterParams {
   name: string;
   email: string;
   password: string;
+  currencyId: number;
 }
 
 export const register = async (params: RegisterParams): Promise<User> => {
   try {
     // await new Promise(resolve => setTimeout(resolve, 3000));
-
     const response = await fetch('https://api.budgetim.ru/users/register', {
       method: 'POST',
       headers: {
