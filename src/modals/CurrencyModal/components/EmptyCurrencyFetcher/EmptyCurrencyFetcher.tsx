f693df8e18f8e660feb103d5a4d09585057a35d1
Vitaly Pavlenko <vitpvln@gmail.com>
2024-03-30 23:52:45 +0100

    upgrade files

--- src/modals/CurrencyModal/components/EmptyCurrencyFetcher/EmptyCurrencyFetcher.tsx
+++ src/modals/CurrencyModal/components/EmptyCurrencyFetcher/EmptyCurrencyFetcher.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useAddCurrency, useGetCurrencies } from '../../../../hooks/currencies';
 import { ScrollView, TouchableHighlight } from 'react-native';
 import { Content, Section } from './styled';
@@ -6,19 +6,58 @@ import { MixedList } from '../../../../components/MixedList';
 import { currencies } from '../../../../constants/currencies';
 import i18n from 'i18n-js';
 import { Loader } from '../../../../components/Loader';
+import { SubscribeWidget } from '../../../../components/SubscribeWidget';
+import { storage } from '../../../../storage';
 
 type EmptyCurrencyFetcherProps = {
   onClose: () => void;
 };
 
+const getFromStorage = () => {
+  return new Promise(resolve => {
+    storage
+      .load({
+        key: 'subscribe',
+        id: 'month',
+      })
+      .then(data => {
+        resolve(data);
+      })
+      .catch(() => {
+        resolve(null);
+      });
+  });
+};
+
 export const EmptyCurrencyFetcher: FC<EmptyCurrencyFetcherProps> = ({ onClose }) => {
   const { data, isLoading } = useGetCurrencies();
+  const [loadingStatus, setLoadingStatus] = useState(true);
+  const [subscribeStatus, setSubscribeStatus] = useState(null);
   const { mutate: addCurrency } = useAddCurrency();
-  if (isLoading) {
+
+  useEffect(async () => {
+    const dataStorage = await getFromStorage();
+    setSubscribeStatus(dataStorage);
+    setLoadingStatus(false);
+  }, []);
+
+  if (isLoading || loadingStatus) {
     return <Loader />;
   }
 
   const currencyKeys = data.map(item => item.title);
+
+  if (data?.length > 2 && !subscribeStatus) {
+    return (
+      <ScrollView>
+        <TouchableHighlight>
+          <Content>
+            <SubscribeWidget />
+          </Content>
+        </TouchableHighlight>
+      </ScrollView>
+    );
+  }
 
   const currenciesList = Object.keys(currencies).filter(key => !currencyKeys.includes(key));
 
